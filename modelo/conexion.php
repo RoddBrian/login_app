20167fa214ccf8bc9c6ceba7aecabe6c78e1a5cf
RoddBrian <roddbrian@outlook.es>
2024-05-08 14:56:44 -0500

    test: :test_tube: Test connection 7

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -9,7 +9,7 @@ $password = 'PbwWtbnbBqxkTaB75KKH';
 $server = 'db-reckonnt-of.cdzyvaae9voe.us-east-1.rds.amazonaws.com';
 $database = 'RECKONNT_SYS';
 
-// $conexion = new mysqli($server, $user, $password, $database);
+$conexion = new mysqli($server, $user, $password, $database);
 
 if ($conexion->connect_errno) {
     echo "Connect failed: %s\n", $conexion->connect_error;
