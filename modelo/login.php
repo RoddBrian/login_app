7040370466dd4a66bca4f3001c643f07261b3a78
RoddBrian <roddbrian@outlook.es>
2024-05-08 12:24:28 -0500

    test: :test_tube: Test db connection

--- modelo/login.php
+++ modelo/login.php
@@ -7,25 +7,26 @@ switch ($_GET["op"]) {
 
     case "validateAccess":
         $data = json_decode(file_get_contents("php://input"), true);
-        if (!empty($data["username"]) || !empty($data["password"])) {
-            $query = "SELECT USER_PASSWORD FROM USERS WHERE USER_NAME = ?";
-            $stmt = $conexion->prepare($query);
-            $stmt->bind_param("s", $data["username"]);
-            $result = $stmt->execute();
-            $password_result = $stmt->get_result()->fetch_object();
-            if(empty($password_result)){
-                echo json_encode(['result' => false, 'detail' => 'Username does not exist']);
-            }else{
-                if($password_result->USER_PASSWORD === $data["password"]){
-                    echo json_encode(['result' => true]);
-                }else{
-                    echo json_encode(['result' => false, 'detail' => 'Incorrect password']);
-                }
-            }
-            $stmt->close();
-        } else {
-            echo json_encode(['result' => false, 'detail' => 'Username or password not provided']);
-        }
+        // if (!empty($data["username"]) || !empty($data["password"])) {
+        //     $query = "SELECT USER_PASSWORD FROM USERS WHERE USER_NAME = ?";
+        //     $stmt = $conexion->prepare($query);
+        //     $stmt->bind_param("s", $data["username"]);
+        //     $result = $stmt->execute();
+        //     $password_result = $stmt->get_result()->fetch_object();
+        //     if(empty($password_result)){
+        //         echo json_encode(['result' => false, 'detail' => 'Username does not exist']);
+        //     }else{
+        //         if($password_result->USER_PASSWORD === $data["password"]){
+        //             echo json_encode(['result' => true]);
+        //         }else{
+        //             echo json_encode(['result' => false, 'detail' => 'Incorrect password']);
+        //         }
+        //     }
+        //     $stmt->close();
+        // } else {
+        //     echo json_encode(['result' => false, 'detail' => 'Username or password not provided']);
+        // }
+        echo json_encode(['host' => getenv('DB_HOST')]);
 
         break;
 
