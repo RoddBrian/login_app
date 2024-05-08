3e27bf4bfd516d02587081fe848a577f5e210031
RoddBrian <roddbrian@outlook.es>
2024-05-08 14:51:00 -0500

    test: :test_tube: Test connection 5

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -4,16 +4,16 @@
 // $server = getenv('DB_HOST');
 // $database = getenv('DB_NAME');
 
-// $user = 'root';
-// $password = 'wElJdLeaFjlRwAfRPAoVzHSRSBuphJAt';
-// $server = 'monorail.proxy.rlwy.net:22021';
-// $database = 'railway';
+$user = 'admin';
+$password = 'PbwWtbnbBqxkTaB75KKH';
+$server = 'db-reckonnt-of.cdzyvaae9voe.us-east-1.rds.amazonaws.com';
+$database = 'RECKONNT_SYS';
 
 // $conexion = new mysqli($server, $user, $password, $database);
 
 // if ($conexion->connect_errno) {
 //     printf("Connect failed: %s\n", $conexion->connect_error);
-echo 'Success test';
+echo 'Success test'.$user.$password.$server.$database;
 //     exit();
 // } 
 // else {
