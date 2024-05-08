23e1070fe7198360f704c02c4f733598a6e42e82
RoddBrian <roddbrian@outlook.es>
2024-05-08 14:46:52 -0500

    test: :test_tube: Test connection 4

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -4,20 +4,21 @@
 // $server = getenv('DB_HOST');
 // $database = getenv('DB_NAME');
 
-$user = 'root';
-$password = 'wElJdLeaFjlRwAfRPAoVzHSRSBuphJAt';
-$server = 'monorail.proxy.rlwy.net';
-$database = 'railway';
+// $user = 'root';
+// $password = 'wElJdLeaFjlRwAfRPAoVzHSRSBuphJAt';
+// $server = 'monorail.proxy.rlwy.net:22021';
+// $database = 'railway';
 
-$conexion = new mysqli($server, $user, $password, $database);
+// $conexion = new mysqli($server, $user, $password, $database);
 
-if ($conexion->connect_errno) {
-    printf("Connect failed: %s\n", $conexion->connect_error);
-    exit();
-} 
+// if ($conexion->connect_errno) {
+//     printf("Connect failed: %s\n", $conexion->connect_error);
+echo 'Success test';
+//     exit();
+// } 
 // else {
 //     printf("Connected");
 // }
 
-$conexion->set_charset("utf8");
+// $conexion->set_charset("utf8");
 ?>
